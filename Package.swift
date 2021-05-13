4e6f383a9e595059a9cf485e8a0400205c389402
Ryan Bubinski <ryanbubinski@gmail.com>
2021-05-13 12:44:21 -0700

    Bump CryptoSwift package dependency version to 1.4.0

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "CryptoEthereumSwift",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS("12")
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -15,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.4.0")),
         .package(url: "https://github.com/vapor-community/copenssl.git", .exact("1.0.0-rc.1")),
         .package(url: "https://github.com/Boilertalk/secp256k1.swift", .upToNextMinor(from: "0.1.0"))
     ],
